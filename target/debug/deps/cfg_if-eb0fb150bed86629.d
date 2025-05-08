503118534f3dd42c45ccce4d7f13c18c98b1eeae
Cihan Sahin <cihan.sahin98@hotmail.com>
2025-05-08 11:12:33 +0200

    fixed warning

---
+++ target/debug/deps/cfg_if-eb0fb150bed86629.d
@@ -0,0 +1,7 @@
+/home/csahin/pytherparsetest/target/debug/deps/libcfg_if-eb0fb150bed86629.rmeta: /home/csahin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
+
+/home/csahin/pytherparsetest/target/debug/deps/libcfg_if-eb0fb150bed86629.rlib: /home/csahin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
+
+/home/csahin/pytherparsetest/target/debug/deps/cfg_if-eb0fb150bed86629.d: /home/csahin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
+
+/home/csahin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs:
---
+++ target/debug/deps/colorchoice-b081cb980704d8d7.d
@@ -0,0 +1,5 @@
+/home/csahin/pytherparsetest/target/debug/deps/libcolorchoice-b081cb980704d8d7.rmeta: /home/csahin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.3/src/lib.rs
+
+/home/csahin/pytherparsetest/target/debug/deps/colorchoice-b081cb980704d8d7.d: /home/csahin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.3/src/lib.rs
+
+/home/csahin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.3/src/lib.rs:
